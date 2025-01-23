00a6c847cd9b42b778cb5bbac26b28e32031280e
Anish Dubey <anishdubey0712@gmail.com>
2025-01-23 23:13:07 +0530

    Changes

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,7 @@
 //DataBase file
+import {Model,Schema} from "mongoose";
+const UserSchema = new Schema({
+    username: {type: String, unique: true},
+    password: {type: String},
+})
+const UserModel = new Model("User",UserSchema);

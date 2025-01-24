b0231f017d5589599330987b04fed65acc6dec97
Anish Dubey <anishdubey0712@gmail.com>
2025-01-24 23:19:55 +0530

    Changes

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,8 @@ async function main() {
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 app.post("/api/v1/signup", async(req, res) => {
   const username = req.body.username;
   const password = req.body.password;

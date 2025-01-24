4967fbf8ca34c6255743e46ef603a1cab8ceadde
Anish Dubey <anishdubey0712@gmail.com>
2025-01-24 23:30:11 +0530

    Changes

--- src/db.ts
+++ src/db.ts
@@ -7,4 +7,10 @@ const UserSchema = new Schema({
 })
 export const UserModel =  mongoose.model("User",UserSchema);
 
-const ContentSchema = new Schema({});
+const ContentSchema = new Schema({
+    title: {type: String},
+    link: {type: String},
+    tags:{type:mongoose.Types.ObjectId, ref:"Tag"},
+    userId:{type:mongoose.Types.ObjectId, ref:"User", required:true}
+
+});

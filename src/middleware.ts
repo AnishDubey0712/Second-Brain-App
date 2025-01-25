b23b1d6d9e5bd2eb230427d7e3ab18f4413fa2da
Anish Dubey <anishdubey0712@gmail.com>
2025-01-25 22:50:04 +0530

    Changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,6 @@
 //Middlewares
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+export const userMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const header = req.headers.authorization;
+}
